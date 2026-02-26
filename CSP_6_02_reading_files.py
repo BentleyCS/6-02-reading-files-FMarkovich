5581e0a818397953ba4a43deb0353c0b5e70cb21
FMarkovich <fmarkovich27@bentleyschool.org>
2026-02-26 11:44:28 -0800

    Add files via upload

--- CSP_6_02_reading_files.py
+++ CSP_6_02_reading_files.py
@@ -12,9 +12,18 @@ def toString(fileName):
 
 def longestLine(fileName):
     #Given a file return the longest line from within that file
-    pass
+    max = 0
+    f=open(fileName)
+    for line in f:
+        if len(line) > max:
+            max = len(line)
+    return line
 
 def toBinary(fileName):
     #Given a file that is only 0's and 1's return a list of the file broken into bytes.
     #An example return might be ['01101001', '00101010', '1010']
-    pass
+    f=open(fileName)
+    bits = ''.join(f.read().split())
+    print ([bits[i:i + 8] for i in range(0, len(bits), 8)])
+    return [bits[i:i + 8] for i in range(0, len(bits), 8)]
+toBinary("6.02 Binary.txt")
