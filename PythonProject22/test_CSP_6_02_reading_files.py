3edfa9902ac9918eb0307cc1595eb01d7d518cdb
gcurrin <97988229+gcurrin@users.noreply.github.com>
2026-02-28 10:17:43 -0800

    Delete PythonProject22 directory

--- PythonProject22/test_CSP_6_02_reading_files.py
+++
@@ -1,5 +0,0 @@
-import CSP_6_02_reading_files as HW
-def test_longestLine():
-    assert HW.longestLine("6.02 Longest Line.txt") == "Felix Markovich"
-def test_toBinary():
-    assert HW.toBinary("6.02 Binary.txt") == ['10101000', '01010101', '00100001', '00101111', '01100010', '1011101']
